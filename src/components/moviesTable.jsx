557fb343f4ab4cfcbdaa2feca459a01cec6dae0e
crisilto <cristinasilvestre1708@gmail.com>
2024-08-26 20:27:09 +0200

    Migrating moviesTable.jsx to Functional Component with Hooks

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import Like from "./common/like";
 import Table from "./common/table";
 
-class MoviesTable extends Component {
-  columns = [
+function MoviesTable({ movies, onSort, sortColumn, onDelete, onLike }) {
+  const columns = [
     { path: "title", label: "Title", content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link> },
     { path: "genre.name", label: "Genre" },
     { path: "numberInStock", label: "Stock" },
@@ -12,14 +12,14 @@ class MoviesTable extends Component {
     {
       key: "like",
       content: (movie) => (
-        <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />
+        <Like liked={movie.liked} onClick={() => onLike(movie)} />
       ),
     },
     {
       key: "delete",
       content: (movie) => (
         <button
-          onClick={() => this.props.onDelete(movie)}
+          onClick={() => onDelete(movie)}
           className="btn btn-danger btn-sm"
         >
           Delete
@@ -28,18 +28,14 @@ class MoviesTable extends Component {
     },
   ];
 
-  render() {
-    const { movies, onSort, sortColumn } = this.props;
-
-    return (
-      <Table
-        columns={this.columns}
-        data={movies}
-        onSort={onSort}
-        sortColumn={sortColumn}
-      />
-    );
-  }
+  return (
+    <Table
+      columns={columns}
+      data={movies}
+      onSort={onSort}
+      sortColumn={sortColumn}
+    />
+  );
 }
 
 export default MoviesTable;
